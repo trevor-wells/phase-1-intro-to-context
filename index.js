9a61d23e0af2bfbc89dfd8838209d807737e9758
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-03-27 16:19:38 -0600

    add createEmployeeRecord, createEmployeeRecords, createTimeInEvent, createTimeOutEvent

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Your code here
+function createEmployeeRecord(employee){
+    const employeeRecord = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord
+}
+
+function createEmployeeRecords(employees){
+    const employeeRecords = []
+    for (const employee of employees) {
+        employeeRecords.push(createEmployeeRecord(employee))
+    }
+    return employeeRecords
+}
+
+function createTimeInEvent(employeeRecord , dateStamp){
+    const [date,hour] = dateStamp.split(" ")
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour , 10),
+        date: date
+    })
+    return employeeRecord
+}
+
+function createTimeOutEvent(employeeRecord , dateStamp){
+    const [date,hour] = dateStamp.split(" ")
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour , 10),
+        date: date
+    })
+    return employeeRecord
+}
+
+function hoursWorkedOnDate(employeeRecord , date){
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(employeeRecord , date){
+    return payOwed
+}
+
+function allWagesFor(employeeRecord){
+    return totalPayOwed
+}
+
+function calculatePayRoll(employeeRecords){
+    return sumOfPay
+}
